096d70683e12da72ab932e6e35f7735636d8c422
Uber - Abdul <abdul@zoojoo.be>
2017-05-05 17:01:15 +0530

    Installed Network Library axios

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
-    
     componentWillMount() {
         console.log('hey');
     }
