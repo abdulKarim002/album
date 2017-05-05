2d8911bac35ff1b93d9de80f8b5bf2bf9a4a63fb
Uber - Abdul <abdul@zoojoo.be>
2017-05-05 16:05:45 +0530

    Initial Commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AlbumList extends Component {
+    
+    componentWillMount() {
+        console.log('hey');
+    }
+    
+ render() {
+    return (
+    <View>
+        <Text>Album List!!!!</Text>
+    </View>
+    );
+ }
+}
+
+export default AlbumList;
